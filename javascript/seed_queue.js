55703ed6f9345ffeec536c520bee7a9253241081
Dario Costa <dariocostapena@gmail.com>
2023-04-06 22:38:02 +0200

    Another approach for seed&prompt retrieval feature. Needs to be tested

--- javascript/seed_queue.js
+++ javascript/seed_queue.js
@@ -1,26 +1,107 @@
+var selectedSeed = null;
+
 function getSeed() {
     var infoDivContent = document.querySelector("#html_info_txt2img").textContent;
     var seedRegex = /Seed:\s+(\d+)/;
     var seedMatch = infoDivContent.match(seedRegex);
     if (seedMatch) {
         var seed = seedMatch[1];
-        var destSeedInput = Array.from(document.querySelectorAll('label span')).find(span => span.textContent === 'Seed(s) (Comma separated)').nextElementSibling;
-        var seeds = destSeedInput.value.split(",").map(s => s.trim());
-        if (seeds.includes(seed)) {
-            alert("The seed already exists in the input.");
+        var promptText = getPromptText();
+
+        // Add the seed and promptText to the hidden input as a JSON object
+        var hiddenInput = document.getElementsByName("hidden_prompt_seed_pairs_input")[0];
+        var seedPromptPairs = hiddenInput.value ? JSON.parse(hiddenInput.value) : [];
+
+        var existingPairIndex = seedPromptPairs.findIndex(pair => pair.seed === seed);
+        if (existingPairIndex > -1) {
+            seedPromptPairs[existingPairIndex].prompt = promptText;
         } else {
-            if (destSeedInput.value.trim() === "") {
-                destSeedInput.value = seed;
-            } else {
-                destSeedInput.value += ", " + seed;
-            }
+            seedPromptPairs.push({seed: seed, prompt: promptText});
         }
+
+        hiddenInput.value = JSON.stringify(seedPromptPairs);
+
+        // Add the seed to the visible list
+        var seedList = document.getElementById("seed_list");
+        var li = document.createElement("li");
+        li.textContent = seed;
+        li.onclick = function () {
+            setSelectedSeed(seed);
+        };
+
+        if (existingPairIndex > -1) {
+            seedList.replaceChild(li, seedList.children[existingPairIndex]);
+        } else {
+            seedList.appendChild(li);
+        }
+
     } else {
         alert("No seed found in the output viewer.");
     }
 }
 
+
 function getPromptText() {
     var promptTextArea = document.querySelector('label textarea.autocomplete');
     return promptTextArea.value;
 }
+
+function setSelectedSeed(seed) {
+    selectedSeed = seed;
+    updateSelectedSeedUI(seed);
+}
+
+function updateSelectedSeedUI(seed) {
+    var seedList = document.getElementById("seed_list");
+    var items = seedList.getElementsByTagName("li");
+    for (var i = 0; i < items.length; i++) {
+        if (items[i].textContent === seed) {
+            items[i].style.backgroundColor = "lightblue";
+        } else {
+            items[i].style.backgroundColor = "";
+        }
+    }
+}
+
+function deleteSeed() {
+    if (selectedSeed !== null) {
+        // Remove the selected seed from the JSON object in the hidden Textbox
+        var hiddenInput = document.getElementsByName("hidden_prompt_seed_pairs_input")[0];
+        var seedPromptPairs = JSON.parse(hiddenInput.value);
+
+        seedPromptPairs = seedPromptPairs.filter(pair => pair.seed !== selectedSeed);
+        hiddenInput.value = JSON.stringify(seedPromptPairs);
+
+        // Remove the selected seed from the list element
+        var seedList = document.getElementById("seed_list");
+        var items = seedList.getElementsByTagName("li");
+        for (var i = 0; i < items.length; i++) {
+            if (items[i].textContent === selectedSeed) {
+                seedList.removeChild(items[i]);
+                break;
+            }
+        }
+
+        // Reset the selectedSeed variable
+        selectedSeed = null;
+    } else {
+        alert("Please select a seed to delete.");
+    }
+}
+
+
+function deleteAllSeeds() {
+    // Clear the JSON object in the hidden Textbox
+    var hiddenInput = document.getElementsByName("hidden_prompt_seed_pairs_input")[0];
+    hiddenInput.value = "";
+
+    // Clear the list element
+    var seedList = document.getElementById("seed_list");
+    while (seedList.firstChild) {
+        seedList.removeChild(seedList.firstChild);
+    }
+
+    // Reset the selectedSeed variable
+    selectedSeed = null;
+}
+
--- scripts/seed_queue.py
+++ scripts/seed_queue.py
@@ -26,14 +26,26 @@ class Script(scripts.Script):
         with gr.Row():
             dest_seed = gr.Textbox(label='Seed(s) (Comma separated)', lines=1)
 
-        # Add a gradio.HTML component with a button
         get_seed_button = gr.HTML("""
             <button id="get-seed-button" class="gradio_button svelte-1v6o9pu" onclick="getSeed()">Get Seed</button>
         """)
 
-        return [dest_seed, enabled, get_seed_button]
+        seed_list = gr.HTML('<ul id="seed_list"></ul>')
 
-    def run(self, p, dest_seed, enabled):
+        delete_seed_button = gr.HTML("""
+            <button id="delete-seed-button" class="gradio_button svelte-1v6o9pu" onclick="deleteSeed()">Delete Seed</button>
+        """)
+
+        delete_all_seeds_button = gr.HTML("""
+            <button id="delete-all-seeds-button" class="gradio_button svelte-1v6o9pu" onclick="deleteAllSeeds()">Delete All Seeds</button>
+        """)
+
+        hidden_prompt_seed_pairs_input = gr.Textbox(name="hidden_prompt_seed_pairs_input", style={"visibility": "hidden"})
+
+        return [dest_seed, enabled, hidden_prompt_seed_pairs_input]
+
+
+    def run(self, p, enabled, hidden_prompt_seed_pairs_input):
         if not enabled:
             return None
 
@@ -42,6 +54,10 @@ class Script(scripts.Script):
         # Force Batch Count and Batch Size to 1.
         p.n_iter = 1
         p.batch_size = 1
+
+        # TODO: PARSE hidden_prompt_seed_pairs_input and retrieve the seeds into dest_seed, and each seed prompt into another variable
+        dest_seed = "1000, 1001"
+        #####
 
         # Manual seeds
         seeds = [int(x.strip()) for x in dest_seed.split(",")]
